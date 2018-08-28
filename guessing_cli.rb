637e1169c86ebf61122a2d5d219fa1e3c795f9e2
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-28 15:41:42 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,17 +4,17 @@ end
 
 def run_guessing_game
   num = rand(1..6)
-  quest = 0
+  quest = nil
   puts "Guess a number between 1 and 6."
-  while quest == 0 
-  quest = gets.chomp
+  while quest == nil 
+    quest = gets.chomp
     if quest == num
       puts "You guessed the correct number!"
     else
       puts "The computer guessed #{num}."
       puts "Exit?"
-      quest2 = gets.chomp
-      exit_game if quest2.downcase == "exit"
+      quest = gets.chomp
+      exit_game if quest.downcase == "exit"
     end 
   end
 end
