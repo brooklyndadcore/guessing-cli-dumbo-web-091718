98e6350673290df49c23ff611ea64a5d76d5bff9
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-28 15:56:04 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,19 +1,19 @@
 
 def run_guessing_game
-  num = rand(1..6)
   quest = nil
-  puts "Guess a number between 1 and 6."
+ 
   while quest == nil 
+    puts "Guess a number between 1 and 6."
     quest = gets.chomp
     guess = quest.to_i
-    if  guess == num
+     num = rand(1..6)
+    if guess == "exit"
+      puts "Goodbye!"
+      break
+    elsif  guess == num
       return "You guessed the correct number!"
     elsif guess != num 
       puts "The computer guessed #{num}."
-      puts "Exit or Play?"
-      quest = gets.chomp
-      return "Goodbye!" if quest == "exit"
-      run_guessing_game if quest == "play"
     end
   end
 end
